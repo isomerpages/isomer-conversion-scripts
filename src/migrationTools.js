52833a4fd9b7080dc1f59de16e4f1ff72098bc7d
kwajiehao <jie_hao_kwa@alumni.brown.edu>
2020-10-06 18:30:23 +0800

    feat: extract notification data from index.md
    Previously, the homepage notification content was not being copied into the
    front matter of the `index.md` file correctly. The site notification content
    was previously in the `index.md` as the markdown content for v1, but for v2
    we need that data in the front matter.
    
    This commit modifies the `homepageModifier` function to add the notification
    content in the front matter.

--- src/migrationTools.js
+++ src/migrationTools.js
@@ -209,7 +209,7 @@ function contactUsModifier(contactUsObject, contactUsMarkdown) {
 // programmes.yml file path
 // as objects, and returns the relevant data needed to modify index.md's
 // front matter
-function homepageModifier(homepageObj, homepageFields, programmesObj) {
+function homepageModifier(homepageObj, homepageFields, notificationContent) {
   // various empty objects to store results
   const sections = [{ hero: {} }];
   const resources = {};
@@ -320,15 +320,30 @@ function homepageModifier(homepageObj, homepageFields, programmesObj) {
     });
   }
 
-  return ({
+  const res = {
     sections,
-  });
+  }
+
+  if (notificationContent) res.notification = notificationContent
+
+  return res;
+}
+
+function extractNotificationContent (indexMd) {
+  const { mdBody: indexMdContent } = utils.frontMatterParser(indexMd)
+  const indexMdContentArr = indexMdContent.split('-->')
+  // Assumption: that the substring `-->` is not used in the notification string
+  const parsedNotificationContent = indexMdContentArr.length > 1 ? indexMdContentArr[1] : indexMdContentArr[0]
+  return parsedNotificationContent
 }
 
 // modify index.md file, which requires homepageModifier
 function indexModifier(homepageFields, homepageObj, programmesObj, indexMd) {
+  // extract notification data if any
+  const notificationContent = extractNotificationContent(indexMd)
+
   // update the homepage yml data
-  const newData = homepageModifier(homepageObj, homepageFields, programmesObj);
+  const newData = homepageModifier(homepageObj, homepageFields, notificationContent);
 
   // update the front matter
   const res = utils.frontMatterInsert(indexMd, newData);
