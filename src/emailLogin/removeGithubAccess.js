306d36d4b465a0ee8a0391a582076877b55b3fdd
Alexander Lee <alexander@open.gov.sg>
2023-03-09 16:16:51 +0800

    feat: add removeGithubAccess script

---
+++ src/emailLogin/removeGithubAccess.js
@@ -0,0 +1,31 @@
+const { Octokit } = require('octokit');
+
+// name of repo
+const REPO = process.argv[2];
+
+const { PERSONAL_ACCESS_TOKEN, GITHUB_ORG_NAME } = process.env;
+
+const octokit = new Octokit({
+  auth: PERSONAL_ACCESS_TOKEN,
+});
+
+const removeGithubAccess = async (site) => {
+  try {
+    await octokit.request(`PUT /orgs/${GITHUB_ORG_NAME}/teams/${site}/repos/${GITHUB_ORG_NAME}/${site}`, {
+      org: GITHUB_ORG_NAME,
+      team_slug: site,
+      owner: GITHUB_ORG_NAME,
+      repo: site,
+      permission: 'pull',
+    });
+    console.log('Set team permission to read only');
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+const main = async () => {
+  await removeGithubAccess(REPO);
+};
+
+main();
