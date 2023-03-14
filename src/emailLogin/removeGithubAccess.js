3c099fcf2bc061417720d911663ca56af4975f31
Alexander Lee <alexander@open.gov.sg>
2023-03-14 13:48:07 +0800

    Chore: add logging for github access

--- src/emailLogin/removeGithubAccess.js
+++ src/emailLogin/removeGithubAccess.js
@@ -1,3 +1,6 @@
+const axios = require('axios');
+const fs = require('fs');
+
 const { Octokit } = require('octokit');
 
 // name of repo
@@ -11,6 +14,30 @@ const octokit = new Octokit({
 
 const removeGithubAccess = async (site) => {
   try {
+    const { data: respData } = await axios.get(
+      `https://api.github.com/orgs/${GITHUB_ORG_NAME}/teams/${site}/members`,
+      {
+        headers: {
+          Authorization: `token ${PERSONAL_ACCESS_TOKEN}`,
+        },
+      },
+    );
+    if (!respData) {
+      console.error(`${site} has no members in the team - please check that the repo name and team name are the same`);
+      return;
+    }
+    const contributorNames = respData
+      .map(({ login }) => login);
+
+    if (!fs.existsSync('./emailMigrationData')) {
+      fs.mkdirSync('./emailMigrationData');
+    }
+    const dirPath = `./emailMigrationData/${site}`;
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath);
+    }
+    fs.writeFileSync(`${dirPath}/githubTeam.txt`, contributorNames.join('\n'));
+
     await octokit.request(`DELETE /orgs/${GITHUB_ORG_NAME}/teams/${site}/repos/${GITHUB_ORG_NAME}/${site}`);
     console.log(`Removing team access for ${site}`);
   } catch (e) {
